2bcf13793ac2dce639f4bb1dd43392aed39e55b0
albertcuesta <acm2291@gmail.com>
2021-01-28 10:31:37 +0100

    Initial Commit

---
+++ Bemobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bemobile
+//
+//  Created by Albert on 28/01/2021.
+//  Copyright © 2021 Albert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
